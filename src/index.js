6856adcb37ed84ca2a4270ec7ef0c64386f34703
ostyyy <ggg468432@gmail.com>
2026-04-25 21:02:01 +0300

    Implement getFactorial function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function factorial(num) {
+    if (!Number.isFinite(num) || !Number.isInteger(num) || num < 0) {
+        return null;
+    }
+
+    if (num === 0 || num === 1) return 1;
+
+    return num * factorial(num - 1);
+}
+const result = factorial(4);
+
+if (!result) {
+    console.log('invalid input!');
+} else {
+    console.log(result);
+}
