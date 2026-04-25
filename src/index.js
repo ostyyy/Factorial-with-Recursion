d1054f7be1c389bf204fa7ceded39073dfd24f08
ostyyy <ggg468432@gmail.com>
2026-04-25 21:03:19 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
-function factorial(num) {
+function getFactorial(num) {
     if (!Number.isFinite(num) || !Number.isInteger(num) || num < 0) {
         return null;
     }
 
     if (num === 0 || num === 1) return 1;
 
-    return num * factorial(num - 1);
+    return num * getFactorial(num - 1);
 }
-const result = factorial(4);
+const result = getFactorial(4);
 
 if (!result) {
     console.log('invalid input!');
